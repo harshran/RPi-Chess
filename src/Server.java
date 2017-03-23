352e0263e81a410c5996c5e367769ec103e666be
Ricky Dam <ricky.dam10@hotmail.com>
2017-03-23 16:52:38 -0400

    Cleaned up the code and added comments

--- src/Server.java
+++ src/Server.java
@@ -11,10 +11,10 @@ public class Server {
 
     }
 
-    // Function to end a message by passing in an ip address as a byte array
+    // Function to send a message by passing in an ip, a port number and a message.
     public String sendMessage(String ip, int port, String message) {
 
-        // Create a sendreceive socket.
+        // Create a sendreceive socket
         try {
             sendreceiveSocket = new DatagramSocket(port);
         } catch(SocketException se) {
@@ -22,42 +22,47 @@ public class Server {
             System.exit(1);
         }
 
-        // Create the packet to send and send it.
+        // Initialize the ip address
         InetAddress address = null;
         try {
             address = InetAddress.getByName(ip);
-            // address = InetAddress.getLocalHost();
         } catch(UnknownHostException uhe) {
             uhe.printStackTrace();
         }
+
+        // Create the send packet
         byte[] messageByte = message.getBytes();
         int sendLength = messageByte.length;
         DatagramPacket sendPacket = new DatagramPacket(messageByte, sendLength, address, port);
-        System.out.println("messageByte: " + messageByte);
-        System.out.println("sendLength: " + sendLength);
-        System.out.println("address: " + address);
-        System.out.println("port: " + port);
+        // Send the packet
         try {
             sendreceiveSocket.send(sendPacket);
         } catch(IOException ioe) {
             ioe.printStackTrace();
         }
+
+        // Print some information for the user to know what's going on
+        System.out.println("messageByte: " + messageByte);
+        System.out.println("sendLength: " + sendLength);
+        System.out.println("address: " + address);
+        System.out.println("port: " + port);
         System.out.println("Packet has been sent to: " + ip);
 
-        // Create the receive packet and wait until it is received.
+        // Create the receive packet
         byte[] receivedMessage = new byte[100];
         int receivedLength = receivedMessage.length;
         DatagramPacket receivedPacket = new DatagramPacket(receivedMessage, receivedLength);
-
+        // Wait until a packet has been received
         try {
             sendreceiveSocket.receive(receivedPacket);
         } catch(IOException ioe) {
             ioe.printStackTrace();
             System.exit(1);
         }
+
         System.out.println("Packet has been received from: " + ip);
 
-        // Print out the message from the received packet
+        // Print out the received message
         receivedLength = receivedPacket.getLength();
         String contents = new String(receivedMessage, 0, receivedLength);
         System.out.println("Received message: " + contents);
